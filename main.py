071488720324f4dba5b5cd283d2c4ce1154e344b
PkuCuipy <1900017785@pku.edu.cn>
2023-04-03 00:59:51 +0800

    2023-04-02 工作存档 - 在整图二值化前, 固定先进行 2x Bilinear 超采样, 为了更好地二值化出合适的 area, 防止零零散散以及某些很细的宋体横画消失 - 在 centroid 对齐平均时, 固定进行 2x Bilinear - 最后相当于对 4x 后的 raster image 进行 vectorization.

--- main.py
+++ main.py
@@ -2,21 +2,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import os
 from tqdm import trange, tqdm
 from collections import namedtuple
 from typing import Iterable
 
-# 设置 plt 窗口大小
-plt.rcParams["figure.figsize"] = (13, 9)
-
-# 读入图片, 转为灰度图, 然后二值化
-grey_img = np.array(cv2.imread("data/test_14.png", cv2.IMREAD_GRAYSCALE))
-bin_threshold, bin_img = cv2.threshold(grey_img, 175, 255, cv2.THRESH_BINARY_INV)   # 前景为白色 (255)
-# plt.imshow(bin_img, cmap="gray")
-
-# 提取所有的连通区域
-nr_labels, label_map, label_stats, label_centroids = cv2.connectedComponentsWithStats(bin_img, connectivity=4)    # 使用 4-连通, 尽可能缩小每个区域
-plt.imshow(label_map, cmap="jet")
+# 存储区域信息
+Area = namedtuple("Area", ["idx", "x", "y", "w", "h", "cx", "cy", "area", "bin_mask", "grey_map", "symbol_id"])
 
 # 计算质心
 def calc_centroid(mat: np.ndarray) -> np.ndarray:
@@ -26,32 +18,13 @@ def calc_centroid(mat: np.ndarray) -> np.ndarray:
     c_j = (rows_sum / np.sum(rows_sum)) @ np.arange(mat.shape[1])
     return np.array([c_j, c_i])
 
-# 保存每个连通区域的信息
-Area = namedtuple("Area", ["idx", "x", "y", "w", "h", "cx", "cy", "area", "bin_mask", "grey_map"])
-areas = []
-for idx in range(1, nr_labels):     # 0 为背景, 忽略
-    x, y, w, h, area = label_stats[idx]
-    view = (slice(y, y + h), slice(x, x + w))           # x 向右, y 向下, 因此 y 才是 i, x 才是 j!
-    bin_mask = (label_map[view] == idx)
-    grey_map = np.where(bin_mask, grey_img[view], 255)  # fixme: 1.这里假设了背景色是 255;  2.被 bin_mask 约束使得遗漏浅色抗锯齿部分
-    centroid = calc_centroid(255 - grey_map)            # 基于灰度图计算, 此前是基于 bin_mask 计算的 (label_centroids[idx] - [x, y])
-    areas.append(Area(idx=idx, x=x, y=y, w=w, h=h, cx=centroid[0], cy=centroid[1], area=area, bin_mask=bin_mask, grey_map=grey_map))
-
-# 绘制每个连通域
-for area in tqdm(areas):
-    plt.gca().add_patch(plt.Rectangle((area.x - 0.5, area.y - 0.5), area.w, area.h, fill=False, edgecolor="r", linewidth=0.5))    # 绘制 bbox
-    plt.gca().add_patch(plt.Circle((area.x + area.cx, area.y + area.cy), radius=0.5, fill=False, edgecolor="g", linewidth=0.5))   # 绘制 centroid
-    cv2.imwrite(f"result/{area.idx}.png", area.grey_map)   # 保存图片
-plt.show()
-
-
 # 亚像素图片移动
 def move_img_subpixel(img: np.ndarray, dx: float, dy: float) -> np.ndarray:
     assert abs(dx) < 1.0 and abs(dy) < 1.0
     h, w = img.shape
     padded = np.pad(img, 1, mode="edge")
     x, y = np.meshgrid(np.arange(1, w + 1, dtype="f4") - dx, np.arange(1, h + 1, dtype="f4") - dy)
-    return cv2.remap(padded, x, y, cv2.INTER_LINEAR)
+    return cv2.remap(padded, x, y, cv2.INTER_NEAREST)
 
 # 测试亚像素图片移动
 def test_move_img_subpixel(img_path, save_folder):
@@ -68,7 +41,7 @@ def move_img_to_target_canvas_with_centroid_aligned_and_up_scaled(a: Area, upsca
     assert canvas_w >= canvas_w_at_least and canvas_h >= canvas_h_at_least, "提供的画布尺寸太小!"
     # 在超采样前, 先在周围围一圈白色
     padded_a = np.pad(a.grey_map, 1, mode='constant', constant_values=(255, 255))
-    upscaled_padded_a = cv2.resize(padded_a, (0, 0), fx=upscale, fy=upscale, interpolation=cv2.INTER_LINEAR)
+    upscaled_padded_a = cv2.resize(padded_a, (0, 0), fx=upscale, fy=upscale, interpolation=cv2.INTER_LINEAR)   # fixme
     # 超采样之后的 centroid 坐标
     upscaled_padded_a_cx = ((a.cx + 1) * upscale + 0.5 * (upscale - 1))     # +1 是因为 padding, * s + 0.5(s-1) 是因为 upscale.
     upscaled_padded_a_cy = ((a.cy + 1) * upscale + 0.5 * (upscale - 1))     # 基于 NEAREST 可以轻易推导 (认为像素坐标点是撑满图片的小方块的中心点). 但对 LINEAR 好像也成立.
@@ -89,7 +62,6 @@ def move_img_to_target_canvas_with_centroid_aligned_and_up_scaled(a: Area, upsca
     can[move_y_int : move_y_int + upscaled_padded_a.shape[0], move_x_int : move_x_int + upscaled_padded_a.shape[1]] = upscaled_padded_a
     # 亚像素级别的移动
     can = move_img_subpixel(can, move_x_f32, move_y_f32)
-    plt.imshow(can); plt.show()
     return can
 
 # 在高分辨率下, 将若干个区域的 centroid 对齐
@@ -104,26 +76,64 @@ def centroids_aligned_mean(areas: Iterable[Area], upscale: int) -> np.ndarray:
 # 测试 centroids_aligned_mean()
 def test_centroids_aligned_mean():
     for test_id, similar_indices in enumerate([
-        # [261,454,506,617,640,169,211,224,247,271,304,423,429,460,515,517,527,528,583,634,643,163,164,410,378,207,317,500,503,649,177],  # `e`
-        # [209,268,350,380,381,416,421,424,536,566,618,],                                                                                 # `a`
-        # [242,243,490,495,497,611,612,616,349,447,347,494,491,446,397,346,244,191,559,555,398,291,292,348,396,492,557,613,614,],         # `t`
-        # [49,110,111,597,112,109,387,541,432,596,],                                                                                      # `,`
-        # [931, 662, 803, 347, 763, 18, 717, 938, 457, 920, 883, ],                                                                       # `研`
         # [54, 337, 1860, 735, 1403, 61, 1549, 305, 1137, 1322, 1604, 1739, 1740, 91, 306, 1406, 1556, 324, 761, 670, 667,675, 962, 1256, 1617, 126, 1816, 484, 473, 1261, 1280, 813, 835, 138, 1678, 1263, 83, 743, 690, 1339, 1027, 49,1383, 1407, 1422, 210, 1241, 1145, 1578, 1759, 810, 832, 1050, 838, 1272, 1445, 1610, 1544, 1068,], # `m`, test_9_down_from_1200
-        [557, 642, 1605, 150, 1091, 213, ],                                                                                              # `as`, test_14
-        [557, 642, 1605, 150, 1091, ],                                                                                              # `as`, test_14
-        [557, 642, 1605, 150, ],                                                                                              # `as`, test_14
-        [557, 642, 1605, ],                                                                                              # `as`, test_14
-        [557, 642, ] ,                                                                                             # `as`, test_14
-        [557, ],                                                                                              # `as`, test_14
+        # [2314, 2076, 2009, 2323, ], # `都`, test_6
+        # [344, 976, 200, 878, 515, ], # `格`, test_15
+        # [75, 74, 546, 435, 511, 489, 481, 140, 47, ], # `在`, test_15
+        # [1528, 201, 996, 1374, 803, 1342, 909, 914, 573, 854, 149, 1408, 725, 553, 437, 224, 828, 1466, 282, 418, 1190, 44, 484, 842, 153, 580, 773, 1297, 1211, 590, 775, 360, 340, 746, 1500, 4, ], # `m`, test_14
     ]):
+        upscale = 2                                                         # 超采样倍率
         similar_areas = [areas[i - 1] for i in similar_indices]
-        grey_result = centroids_aligned_mean(similar_areas, upscale=4)
-        plt.imshow(grey_result); plt.show()                                 # 灰度图
-        cv2.imwrite(f"test_centroids_aligned_mean_{test_id}_{len(similar_indices)}.png", grey_result)
-        plt.imshow(grey_result > 128, cmap="gray"); plt.show()              # 二值图
+        grey_result = centroids_aligned_mean(similar_areas, upscale=upscale)
+        plt.imshow(grey_result); plt.show()
+        cv2.imwrite(f"test_centroids_aligned_mean_{test_id}_{len(similar_indices)}_up={upscale}.png", grey_result)
 
-test_centroids_aligned_mean()
+# ======================================================================================================================
+
+if __name__ == "__main__":
+
+    # 创建存放结果的文件夹
+    if not os.path.exists("result"):
+        os.mkdir("result")
+
+    # 设置 plt 窗口大小
+    plt.rcParams["figure.figsize"] = (13, 9)
+
+    # 读入图片, 转为灰度图, 然后二值化
+    grey_img = np.array(cv2.imread("data/test_14.png", cv2.IMREAD_GRAYSCALE))
+    grey_img = cv2.resize(grey_img, (0, 0), fx=2, fy=2, interpolation=cv2.INTER_LINEAR) # 在这里放大, 而不是在 align_centroids() 中, 因为这有利于提取区域(?)
+    bin_threshold, bin_img = cv2.threshold(grey_img, 175, 255, cv2.THRESH_BINARY_INV)   # 前景为白色 (255)
+    plt.imshow(bin_img, cmap="gray")
+    plt.show()
+
+    # 提取所有的连通区域
+    nr_labels, label_map, label_stats, label_centroids = cv2.connectedComponentsWithStats(bin_img, connectivity=8)    # fixme: 使用 4-连通, 尽可能缩小每个区域, 似乎更适合英文; 8-连通似乎适合中文
+    plt.imshow(label_map, cmap="jet")
+
+    # 保存每个连通区域的信息
+    areas = []
+    for idx in range(1, nr_labels):     # 0 为背景, 忽略
+        x, y, w, h, area = label_stats[idx]
+        view = (slice(y, y + h), slice(x, x + w))           # x 向右, y 向下, 因此 y 才是 i, x 才是 j!
+        bin_mask = (label_map[view] == idx)
+        grey_map = np.where(bin_mask, grey_img[view], 255)  # fixme: 1.这里假设了背景色是 255;  2.被 bin_mask 约束使得遗漏浅色抗锯齿部分
+        centroid = calc_centroid(255 - grey_map)            # 基于灰度图计算, 此前是基于 bin_mask 计算的 (label_centroids[idx] - [x, y])
+        areas.append(Area(idx=idx, x=x, y=y, w=w, h=h, cx=centroid[0], cy=centroid[1], area=area, bin_mask=bin_mask, grey_map=grey_map, symbol_id=None))
+
+    # 绘制每个连通域
+    for area in tqdm(areas):
+        plt.gca().add_patch(plt.Rectangle((area.x - 0.5, area.y - 0.5), area.w, area.h, fill=False, edgecolor="r", linewidth=0.5))    # 绘制 bbox
+        # plt.gca().add_patch(plt.Circle((area.x + area.cx, area.y + area.cy), radius=0.5, fill=False, edgecolor="g", linewidth=0.5))   # 绘制 centroid
+        cv2.imwrite(f"result/{area.idx}.png", area.grey_map)   # 保存图片
+    plt.show()
+
+    # test_centroids_aligned_mean()
+
+    # 2023-04-03 TODO:
+    # 1. 创建 symbol_table, 每个 area 的 symbol_id 指向某个符号, 这个符号由 (grey_map, cx, cy) 表征, 其中 cx, cy 应该都是 int + 0.5 的样子 (因为 2x 超分后 canvas 是偶数边长的, 然后 centroid() 返回的 area 是重心中心化的).
+    # 2. 构建一个 4x 大的空白图片, 把每个符号都放到这个图片上.
+    # 3. 把每个符号的矢量勾勒出来, 构建一个 svg, 把每个符号放到 svg 上, 导出 svg.
+
 
 
 
