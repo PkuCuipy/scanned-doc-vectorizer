0986e73fdd44438172e153bb7e556f4dcfea4d46
PkuCuipy <1900017785@pku.edu.cn>
2023-05-08 21:04:46 +0800

    - 支持一个字体文件的多个子字体, 比如 .ttc (TrueType Collection) - 支持随机采样, 否则像 Songti 这种包含那么多 sub_font, 每个 sub_font 又包含几千个汉字, 那是真的太多了... 电脑卡死.. macOS 的文件管理系统估计也要背锅.

--- nmc/make_svg.py
+++ nmc/make_svg.py
@@ -8,23 +8,42 @@ Ref:
 - https://fontforge.org/docs/scripting/python/fontforge.html
 - FSType: https://fontforge.org/docs/faq.html
 - fstypepermitted: https://fontforge.org/docs/scripting/python/fontforge.html
+- Unicode区段: https://zh.wikipedia.org/wiki/Unicode%E5%8D%80%E6%AE%B5
 """
 
 import fontforge
 from pathlib import Path
+import random
 
 font_folder = Path("./font/")
 font_paths = font_folder.glob("*.*")
 svg_output_folder = Path('./svg/')
 
 for font_path in font_paths:
-    font = fontforge.open(str(font_path), fstypepermitted_flag := 0x1)
-    font.selection.all()
-    for glyph in font.selection.byGlyphs:
-        if not glyph.foreground.isEmpty():  # 空的图就不导出为 SVG 了
-            glyph_filename = svg_output_folder / f'{font.fontname}__{glyph.encoding}__U+{glyph.unicode:04X}.svg'
-            glyph.export(str(glyph_filename))
-        # else:  # debug: 确保没导出的都是空的图
-        #     glyph.export(str(svg_output_folder / f'~~~{font.fontname}__{glyph.encoding}__U+{glyph.unicode:04X}.svg'))
-    print(font.fontname, "done.")
-    font.close()
+    print(list(fontforge.fontsInFile(str(font_path))))
+    for sub_font in fontforge.fontsInFile(str(font_path)):
+        print(f"\n\n读取 {font_path}({sub_font}):")
+        font = fontforge.open(f"{font_path}({sub_font})", fstypepermitted_flag := 0x1)
+
+        # ASCII 部分全都要
+        font.selection.select(("ranges", None), 0, 0x7F)
+        ascii_glyphs = list(font.selection.byGlyphs)
+        for glyph in ascii_glyphs:
+            if not glyph.foreground.isEmpty():  # 空的图就不导出为 SVG 了
+                glyph_filename = svg_output_folder / f'{font.fontname}__{glyph.encoding}__U+{glyph.unicode:04X}.svg'
+                glyph.export(str(glyph_filename))
+        print(f"输出了 {len(ascii_glyphs)} 个 ASCII 字符")
+
+        # 其他部分随机抽取 nr_samples 个
+        nr_samples = 10
+        font.selection.invert()
+        else_glyphs = list(font.selection.byGlyphs)
+        else_glyphs = random.sample(else_glyphs, k=min(nr_samples, len(else_glyphs)))
+        for glyph in else_glyphs:
+            if not glyph.foreground.isEmpty():  # 空的图就不导出为 SVG 了
+                glyph_filename = svg_output_folder / f'{font.fontname}__{glyph.encoding}__U+{glyph.unicode:04X}.svg'
+                glyph.export(str(glyph_filename))
+        print(f"输出了 {len(else_glyphs)} 个其他字符")
+
+        print(font.fontname, "done.")
+        font.close()
